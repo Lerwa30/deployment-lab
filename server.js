44939fbf95a8678c98cce3628530a394bc0d12e2
Leroy <leroypawlick@gmail.com>
2023-03-27 16:47:51 -0700

    change folder

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.listen(4020, () => console.log('server running on port 4020'));
